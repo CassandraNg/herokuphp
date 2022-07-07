84abd54db06bb04daf2ccd03b9b2ad33f1c10fb2
CassandraNg <20017476@myrp.edu.sg>
2022-07-07 12:20:44 +0800

    update

--- web/test.php
+++ web/test.php
@@ -1,7 +1,9 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <html>
 <head>
+    <title>Test page</title> 
 </head>
 <body>
-  Hello World!!
+  <h1>Hello World!!</h1>
 </body>
 </html>
